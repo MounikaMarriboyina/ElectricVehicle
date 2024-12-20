6156531bc3890b7d4555a0a2209692f0637bd6fb
MounikaMarriboyina <mounika.marriboyina@gmail.com>
2024-12-20 19:00:26 +0530

    final code

---
+++ app/src/main/java/com/project/ecorangeestimater/model/EvStation.kt
@@ -0,0 +1,12 @@
+package com.project.ecorangeestimater.model
+
+data class EvStation(
+    val id:Int,
+    val stationName: String,
+    val stationMobile: String,
+    val location: String,
+    val chargingpoints: String,
+    val type: String,
+    val role: String,
+    val adapterTypes: List<String>
+)
